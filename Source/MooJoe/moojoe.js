a1e4f41e5b21c55c9c8948e609ed9291848e1b5c
lee@heungsub.net <lee@heungsub.net@72ee2c20-bd54-0410-81e4-e17a5f4abe1c>
2008-08-19 17:23:04 +0000

    * `DetachedObject`, `AttachedObject` 구현  * 생성자가 DOM 엘리먼트 객체가 아닌 속성값을 받도록 수정  * Specs 페이지 예제 잘 보이도록 스타일 적용  * `ComplexMapping` 일단 제외  * getter, setter 구현, 이걸 이용해서 DOM과 동기화 하도록 수정

--- Source/MooJoe/moojoe.js
+++ Source/MooJoe/moojoe.js
@@ -1,9 +1,142 @@
 MooJoe = {
 	Class: new Class({
-		initialize: function(definitions) {
-			return new Class({
-				initialize: function(element) {
+		initialize: function(rules, properties) {
+			rules = $H(rules);
+			var reflections = {};
+
+			rules.each(function(rule, property) {
+				var element, p = 'html', t = String;
+
+				if($type(rule) == 'string')
+					element = rule;
+
+				else if($type(rule) == 'array') {
+					element = rule.shift();
+
+					rule = rule.link({
+						type: function(i) {
+							return Native.type(i) || Class.type(i);
+						},
+						property: String.type
+					});
+
+					if(rule.property) p = rule.property;
+					if(rule.type) t = rule.type;
+				}
+
+				var ref = reflections[property] = {
+					element: element, property: p, type: t
+				};
+
+				properties['get ' + property] = function() {
+					if(this.properties[property])
+						return this.properties[property];
+
+					else if(this.isAttached()) {
+						var value = this.getMapped(property).get(ref.property);
+						if(this.isEmpty) this.set(property, value);
+						return value;
+					}
+				}
+
+				properties['set ' + property] = function(value) {
+					if(this.isAttached())
+						this.getMapped(property).set(ref.property, value);
+
+					return this.properties[property] = value;
+				}
+			}, this);
+
+			var moojoe_class = new Class({
+
+				initialize: function() {
+					this.properties = {};
+					this.isEmpty = !arguments.length;
+
+					if(Function.type(properties.initialize))
+						properties.initialize.run(arguments, this);
+
+					else {
+						var rule_names = rules.getKeys();
+						$A(arguments).each(function(arg, i) {
+							this.properties[rule_names[i]] = arg;
+						}, this);
+					}
+				},
+
+				get: function(property) {
+					if(Function.type(properties['get ' + property]))
+						return properties['get ' + property].bind(this)();
+					else
+						return this.properties[property];
+				},
+
+				set: function(property, value) {
+					if(Function.type(properties['set ' + property]))
+						return properties['set ' + property].run(value, this);
+					else
+						return this.properties[property] = value;
+				},
+
+				attach: function(element) { // Thanks to xymfonii
 					this.element = $(element);
+
+					rules.each(function(rule, property) {
+						if(this.isEmpty) this.get(property);
+						else this.set(property, this.get(property));
+					}, this);
+
+					this.isEmpty = true;
+
+					return this.element;
+				},
+
+				detach: function() {
+					var el = this.element;
+					this.element = false;
+					return el;
+				},
+
+				isAttached: function() {
+					return Element.type(this.element);
+				}
+			});
+
+			moojoe_class.rules = rules;
+			moojoe_class.properties = properties;
+
+			moojoe_class.implement({
+				getMapped: function(property) {
+					return this.element.getElement(
+						reflections[property].element
+					);
+				}
+			});
+
+			return moojoe_class;
+		}
+	})
+};
+
+Element.implement({
+	toObject: function(moojoe_class) {
+		var o = new moojoe_class();
+		o.attach(this);
+
+		return o;
+	}
+});
+
+Elements.implement({
+	toObject: function(moojoe_class) {
+		return this.map(function(element) {
+			return element.toObject(moojoe_class);
+		});
+	}
+});
+
+
+/*					this.element = $(element);
 					this.reflections = {};
 
 					$H(definitions).each((function(rule, property) {
@@ -43,35 +176,4 @@ MooJoe = {
 							);
 
 					}).bind(this));
-				},
-
-				get: function(property) {
-					return this[property];
-				},
-
-				set: function(property, value) {
-					this[property] = value;
-
-					var ref = this.reflections[property];
-					ref.element.set(ref.property, value);
-
-					return value;
-				}
-			});
-		}
-	})
-};
-
-Element.implement({
-	toObject: function(moojoe_class) {
-		return new moojoe_class(this);
-	}
-});
-
-Elements.implement({
-	toObject: function(moojoe_class) {
-		return this.map(function(element) {
-			return element.toObject(moojoe_class);
-		});
-	}
-});
+				},*/
